5bf69c48ae059f506eaf4cbc8df5a2921b252e39
BlueLambAsia <swd.kenneth@gmail.com>
2026-05-21 11:54:13 +0800

    Fix API export format

--- api/generateName.js
+++ api/generateName.js
@@ -1,4 +1,4 @@
- export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
@@ -82,4 +82,4 @@ Important:
         console.error('Error:', error);
         return res.status(500).json({ error: error.message });
     }
-}
+};
